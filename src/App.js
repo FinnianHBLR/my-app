9f6ff2f2c77f87298000f82afbd7ce92c69ed864
Finnian-LR <finnian@fingermarkglobal.com>
2021-03-11 19:50:23 +1300

    updated orignal useage of component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//NOTE. You have to use className not class.
+//Onclick is with a capital C.
+class App extends Component {
+  //State is managed from inside compontent. This must extend component. Be careful with states.
+  state = {
+    persons: [
+      { name: 'Max', age: 28 },
+      { name: 'Manu', age: 29 },
+      { name: 'Stephanie', age: 26 }
+    ],
+    otherState: 'Some other data'
+  }
+
+  switchNameHandler = () => {
+    //console.log('Was clicked.');
+    //DONT DO THIS: this.state.persons[0].name = "MaxiBoy";
+    this.setState({
+      persons: [
+        { name: 'MaxiBoy', age: 28 },
+        { name: 'Manu', age: 29 },
+        { name: 'Stephanie', age: 27 }
+      ]
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Hi, I'm React</h1>
+        <p>This is really working.</p>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+      </div>
+    );
+
+    //Will be compiled to -- return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'testing.'));
+
+  }
 }
 
 export default App;
---
+++ src/Person/Person.js
@@ -0,0 +1,18 @@
+//We do not need component as we are not suing a class that enxtends component.
+import React from 'react';
+
+//JS code has to be run with {}. This is NOT JSX, only one line expressions. You could call a function though.
+//{} Means dynamic
+//Children is a reserved word.
+
+const person = (props) => {
+    return (
+        <div>
+            <p>I'm a {props.name} and I am {props.age} years old</p>
+            <p>{props.children}</p>
+        </div>
+    )
+
+};
+
+export default person;
